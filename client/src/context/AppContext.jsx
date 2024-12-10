2e3181eed029b9e7980931e0129c3887c1bc9ad9
nhattien <nhattien1245@gmail.com>
2024-12-10 23:11:53 +0700

    Implement API call for classifying comments

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,28 +1,30 @@
-import React, { createContext } from 'react';
-// import axios from 'axios';
+import { createContext } from "react";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const send = async (inputText) => {
-    try {
-      //const response = await axios.post('http://localhost:8386/', {
-        //inputText,
-      //});
-      //return response.data;
-      return {
-        sentiment: 1,
-        explain: inputText
-      };
-    } catch (error) {
-      console.error('Error sending data:', error);
-      throw error;
-    }
-  };
+	const send = async (inputText) => {
+		try {
+			const response = await axios.post(
+				`${import.meta.env.VITE_APP_API_URL}/classify-comment`,
+				{},
+				{
+					params: {
+						text: inputText,
+					},
+				}
+			);
+			console.log(response.data);
+			return {
+				sentiment: response.data.predict,
+				explain: inputText,
+			};
+		} catch (error) {
+			console.error("Error sending data:", error);
+			throw error;
+		}
+	};
 
-  return (
-    <AppContext.Provider value={{ send }}>
-      {children}
-    </AppContext.Provider>
-  );
+	return <AppContext.Provider value={{ send }}>{children}</AppContext.Provider>;
 };
